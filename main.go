ef5ee05f41a858751da760bc671650f210f77bd8
RIDER <tdaria230@gmail.com>
2026-07-12 22:44:17 +0300

    Test lms polimorfism

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
-import (
-	"fmt"
-)
+
+import "fmt"
+
+type Animal interface {
+	Speak() string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (d *Dog) Speak() string {
+	return "Woof!"
+}
+
+type Cat struct {
+	Name string
+}
+
+func (c *Cat) Speak() string {
+	return "Meow!"
+}
+
+type Cow struct {
+	Name string
+}
+
+func (c *Cow) Speak() string {
+	return "Moo!"
+}
+
 func main() {
-	fmt.Println("YO! Buy chips to me!")
+	animals := []Animal{
+		&Dog{"Fido"},
+		&Cat{"Fluffy"},
+		&Cow{"Betsy"},
+	}
+
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
 }
