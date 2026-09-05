c7d313292c33252320be612e303e543ba5ae4289
RIDER <tdaria230@gmail.com>
2026-09-05 08:27:51 +0300

    btn added

--- Project1/go.mod
+++ Project1/go.mod
@@ -1,5 +1,3 @@
 module wardrobe
 
 go 1.26.5
-
-require fyne.io/fyne/v2 v2.8.0 // indirect
--- Project1/main.go
+++ Project1/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
 
-type Calculator struct {
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Calcul struct {
 	CurrentInput  string  // Число, которое пользователь ввоодит
 	Display       string  // Число, которое горит на экране(ввод/результат)
 	PreviousValue float64 // Первое число, которое было введено до начала операций
@@ -10,7 +15,23 @@ type Calculator struct {
 	IsNewInput    bool    // Надо ли стереть число(превышает ли его длина 8 цифр)
 }
 
-func (v *Calculator) getting() {
-	fmt.Scan(&v.CurrentInput)
-	v.Display = v.CurrentInput
+func main() {
+	ap := app.New()
+	window := ap.NewWindow("Calculator")
+
+	label := widget.NewLabel("0")
+
+	btn1 := widget.NewButton("1", func() {
+		label.SetText("1")
+	})
+
+	btn1, err := strconv.Atoi("1")
+	if err != nil {
+
+	}
+
+	window.SetContent(widget.NewVBox(label, btn1))
+
+	window.Show()
+	ap.Run()
 }
